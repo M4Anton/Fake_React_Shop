d7c4b798f948e5bcc97c0097e6ed3f9cf1cf4973
Maciek <maciek.antonczak@wp.pl>
2020-11-12 23:29:09 +0100

    initial commit w/ homepage

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'assets/css/style.css';
+
+import { Homepage } from "views";
+
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
